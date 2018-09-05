3a37c7e8ca3907e000ab969ee8bb0cf30a176dd8
Jul Jen <juliajen.prog@gmail.com>
2018-09-05 02:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,17 +8,10 @@ function lowerCaseDrivers(drivers) {
 //  takes an array of drivers with their first and last name separated by a space, and returns an array of JavaScript objects with firstName and lastName attributes.
 
 function nameToAttributes (fullName) {
-  return fullName.map(
-    driver => Object.assign({firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]});
-  )
+  return fullName.map(function (driver) {
+    return { firstName: driver.split(" ")[0], lastName: driver.split(" ")[1] }
+  })
 }
-
-// solution#1 passes
-// function nameToAttributes (fullName) {
-//   return fullName.map(function (driver) {
-//     return { firstName: driver.split(" ")[0], lastName: driver.split(" ")[1] }
-//   })
-// }
 
 // takes an array of drivers as JavaScript objects and returns a string saying "<NAME OF DRIVER> is from <HOMETOWN>" for each JavaScript object in the array.
 
