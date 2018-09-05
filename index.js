b2e147aab4c1ad90bd81db70126b3197c79a34a2
Jul Jen <juliajen.prog@gmail.com>
2018-09-05 02:19:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function nameToAttributes (fullName) {
 // takes an array of drivers as JavaScript objects and returns a string saying "<NAME OF DRIVER> is from <HOMETOWN>" for each JavaScript object in the array. Note that between the < and > we are looking for the value stored in the name and hometown object attributes, not the strings "DRIVER" or "HOMETOWN".
 function attributesToPhrase () {
   return fullName.map(function (driver) {
-    const phrase = is from
-    return { driver.name + phrase + driver.hometown }
+    return ( driver.name + phrase + driver.hometown)
   })
 }
 
